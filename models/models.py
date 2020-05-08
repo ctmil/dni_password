3c2348a1d36bcd8c3e8170779c1944d2f808ab6a
Ignacio Buioli <ibuioli@gmail.com>
2020-05-08 16:57:13 -0300

    feat: button for get DNI

--- models/models.py
+++ models/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class DniPassword(models.Model):
 	_inherit = 'res.users'
+
+	def dni_password(self):
+		if self.partner_id.dni:
+			self.password = self.partner_id.dni
+		else:
+			raise ValidationError('Este usuario no tiene DNI cargado')
