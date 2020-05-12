cd4f95ed7a9b5293b2c1ad07818812784270f99f
Ignacio Buioli <ibuioli@gmail.com>
2020-05-12 14:16:44 -0300

    feat: create button in partner

--- models/models.py
+++ models/models.py
@@ -11,3 +11,21 @@ class DniPassword(models.Model):
 			self.password = self.partner_id.dni
 		else:
 			raise ValidationError('Este usuario no tiene DNI cargado')
+
+class DniPasswordPartner(models.Model):
+	_inherit = 'res.partner'
+
+	def create_user(self):
+
+		if not self.dni:
+			raise ValidationError('Se requiere DNI para crear un usuario')
+		if not self.email:
+			raise ValidationError('Se requiere Email para crear un usuario')
+
+		self.env['res.users'].create({
+			'name': self.name,
+			'login': self.email,
+			'password': self.dni,
+			'partner_id': self.id,
+			'in_group_9': True
+		})
